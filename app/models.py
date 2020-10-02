d6f0e76b6eb7e5d602a5138727f93c00e28dd54f
Felipe Bueno <felipebueno181@gmail.com>
2020-10-02 14:54:51 -0300

    incluindo logica de login

--- app/models.py
+++ app/models.py
@@ -1,16 +1,25 @@
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from app import db
 from sqlalchemy.ext.declarative import DeclarativeMeta
+from flask_login import UserMixin
+from app import login
 
 BaseModel: DeclarativeMeta = db.Model
 
-class User(BaseModel):
+class User(UserMixin, BaseModel):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
     
@@ -22,3 +31,7 @@ class Post(BaseModel):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+    
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
